66d25353d51083c54ddcc6f648fe9e9554720427
Benito <benito.gomez@idnerd.com>
2020-06-19 14:58:57 +0800

    share storage connection and connections pool

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate base64;
 extern crate pretty_env_logger;
 use base64::encode;
-use futures::future;
-use futures::{Future, FutureExt, StreamExt};
+use futures::{FutureExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
@@ -21,11 +20,12 @@ struct Object {
     data: String,
 }
 
+// Vector of connections
 type Connections = Vec<mpsc::UnboundedSender<Result<Message, warp::Error>>>;
+// Hash map key -> active connections
 type Pool = HashMap<String, Connections>;
-type Pools = Arc<Mutex<Vec<Pool>>>;
-
-// type Pool = []Users
+// ??
+type Pools = Arc<Mutex<Pool>>;
 
 fn now() -> u128 {
     let start = SystemTime::now();
@@ -33,120 +33,112 @@ fn now() -> u128 {
     match result {
         Ok(content) => content.as_nanos(),
         Err(error) => {
-            error!(
-                "this is not the way, the writter failed to insert: {0}",
-                error.to_string()
-            );
+            error!("the writter failed to insert: {0}", error.to_string());
             0
         }
     }
 }
 
-fn ws_connected(ws: WebSocket, _pools: Pools) -> impl Future<Output = Result<(), ()>> {
-    // Use a counter to assign a new unique ID for this user.
-
-    // Split the socket into a sender and receive of messages.
-    let (user_ws_tx, user_ws_rx) = ws.split();
+async fn ws_reader(ws: WebSocket, data: Object) {
+    let (user_ws_tx, mut user_ws_rx) = ws.split();
 
     // Use an unbounded channel to handle buffering and flushing of messages
     // to the websocket...
     let (_tx, rx) = mpsc::unbounded_channel();
     tokio::task::spawn(rx.forward(user_ws_tx).map(|result| {
         if let Err(e) = result {
-            eprintln!("websocket send error: {}", e);
+            error!("websocket send error: {}", e);
         }
     }));
 
-    // Save the sender in our list of connected users.
-    // users.lock().unwrap().insert(0, tx);
+    let j = serde_json::to_string(&data);
+    _tx.send(Ok(Message::text(j.unwrap()))).ok();
 
-    // Return a `Future` that is basically a state machine managing
-
-    // Make an extra clone to give to our disconnection handler...
-    // let users2 = users.clone();
-
-    user_ws_rx
-        // Every time the user sends a message, broadcast it to
-        // all other users...
-        .for_each(move |_msg| {
-            // user_message(my_id, msg.unwrap(), &users);
-            future::ready(())
-        })
-        // for_each will keep processing as long as the user stays
-        // connected. Once they disconnect, then...
-        .then(move |result| {
-            // user_disconnected(my_id, &users2);
-            future::ok(result)
-        })
-    // If at any time, there was a websocket error, log here...
-    // .map_err(move |e| {
-    //     eprintln!("websocket error(uid={}): {}", my_id, e);
-    // })
+    // TODO: add connection to the corresponding pool
+    while let Some(result) = user_ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                // TODO: remove connection on disconnect
+                eprintln!("websocket error: {}", e);
+                break;
+            }
+        };
+        // user_message(my_id, msg, &users).await;
+        info!("received: {:?}", msg);
+    }
 }
 
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-
+fn storage() -> Arc<Mutex<sled::Db>> {
     // Database
-    let t = sled::open("db").unwrap();
+    let db = sled::open("db").unwrap();
     let test = Object {
         created: now(),
         updated: 0,
         data: encode("{\"data\": \"🎁\"}"),
     };
     let serialized = serde_json::to_string(&test).unwrap();
-    let result = t.insert("test", serialized.as_bytes());
+    let result = db.insert("test", serialized.as_bytes());
     let _status = match result {
         Ok(_content) => {
-            info!("such info");
+            info!("sled success");
         }
         Err(error) => {
-            error!(
-                "this is not the way, the writter failed to insert: {0}",
-                error.to_string()
-            );
+            error!("sled failed to insert: {0}", error.to_string());
         }
     };
 
+    return Arc::new(Mutex::new(db));
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let db = storage();
     // pool
-    // Keep track of all connected users, key is usize, value
+    // Keep track of connections, key is usize, value
     // is a websocket sender.
-    let pools = Arc::new(Mutex::new(vec![]));
-    // Turn our "state" into a new Filter...
-    let pools = warp::any().map(move || pools.clone());
+    let pools: Pools = Arc::new(Mutex::new(Pool::new()));
+
+    // https://users.rust-lang.org/t/why-closure-outlives-variables-owned-by-main/16725
+    // even if the variable is Arc<Mutex> you need to clone?
+    let subscription_pools = pools.clone();
+    let rest_db = db.clone();
 
     // routes
-    let ws = warp::path::param()
+    let subscription = warp::path::param()
         // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(|key: String, ws: warp::ws::Ws| {
-            // And then our closure will be called when it completes...
-            info!("Hello {}!", key);
-            ws.on_upgrade(|websocket| {
-                // Just echo all messages back...
-                let (tx, rx) = websocket.split();
-                rx.forward(tx).map(|result| {
-                    if let Err(e) = result {
-                        eprintln!("websocket error: {:?}", e);
-                    }
-                })
-            })
-        });
-
-    let clock = warp::path::param()
-        // The `ws()` filter will prepare the Websocket handshake.
-        .and(warp::ws())
-        .and(pools)
-        .map(|key: String, ws: warp::ws::Ws, pools: Pools| {
-            info!("Hello {}!", key);
-            ws.on_upgrade(move |websocket| {
-                ws_connected(websocket, pools).map(|result| result.unwrap())
-            })
+        .map(move |key: String, ws: warp::ws::Ws| {
+            let mut data = Object {
+                created: 0,
+                updated: 0,
+                data: "".to_string(),
+            };
+            if key == "" {
+                // Clock connection
+                info!("subscription to the clock");
+                data = Object {
+                    created: 0,
+                    updated: 0,
+                    data: now().to_string(),
+                };
+            } else {
+                info!("subscription on {}!", key);
+                let result = db.lock().unwrap().get(key).unwrap();
+                if result.is_some() {
+                    let raw = &result.unwrap();
+                    let serialized = String::from_utf8_lossy(raw);
+                    data = serde_json::from_str(&serialized).unwrap();
+                }
+            }
+            info!("pool {}", subscription_pools.lock().unwrap().len());
+            ws.on_upgrade(|websocket| ws_reader(websocket, data))
         });
 
     let rest = warp::path::param().map(move |key: String| {
-        let result = t.get(key).unwrap();
+        let result = rest_db.lock().unwrap().get(key).unwrap();
         if result.is_some() {
             let raw = &result.unwrap();
             let serialized = String::from_utf8_lossy(raw);
@@ -161,9 +153,9 @@ async fn main() {
         }
     });
 
-    let keys = warp::any().map(|| format!("Hello!"));
+    let keys = warp::any().map(|| format!("TODO: get keys from db"));
 
-    let routes = ws.or(clock).or(rest).or(keys);
+    let routes = subscription.or(rest).or(keys);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
---
+++ tests/http.rs
@@ -0,0 +1,7 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*;
+use std::io::Write;
+use std::process::Command; // Run programs // Used for writing assertions
+
+#[test]
+fn startServer() -> Result<(), Box<dyn std::error::Error>> {}
