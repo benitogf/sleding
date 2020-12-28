52862ec656c4eeaad463ea669e290d73712c6944
Benito <benito.gomez@idnerd.com>
2020-12-28 16:55:07 +0800

    clock subscription, post test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate base64;
 extern crate pretty_env_logger;
-use base64::encode;
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -10,6 +9,8 @@ use std::sync::{
 };
 use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::sync::mpsc;
+use tokio::time::delay_for;
+use tokio::time::Duration;
 use warp::ws::{Message, WebSocket};
 use warp::{http, Filter};
 
@@ -47,6 +48,61 @@ fn now() -> u128 {
   return start.duration_since(UNIX_EPOCH).unwrap().as_nanos();
 }
 
+async fn clock_writer(pools: Pools) {
+  let key = "".to_string();
+  let pkey = key.clone();
+
+  loop {
+    let mut mpools = pools.lock().await;
+    let pool = mpools.get_mut(&pkey).unwrap();
+    for (_index, connection) in pool.iter() {
+      connection.send(Ok(Message::binary(now().to_string()))).ok();
+    }
+    drop(mpools);
+    delay_for(Duration::from_millis(1000)).await;
+  }
+}
+
+async fn clock_reader(ws: WebSocket, pools: Pools) {
+  let key = "".to_string();
+  let pkey = key.clone();
+  let exit_key = key.clone();
+  let connection_id = NEXT_CONNECTION_ID.fetch_add(1, Ordering::Relaxed);
+
+  info!("subscription to the clock");
+
+  let (ws_tx, mut ws_rx) = ws.split();
+  // Use an unbounded channel to handle buffering and flushing of messages
+  // to the websocket...
+  let (tx, rx) = mpsc::unbounded_channel();
+  tokio::task::spawn(rx.forward(ws_tx));
+  tx.send(Ok(Message::binary(now().to_string()))).ok();
+  info!("clock handshake sent");
+
+  // add connection to the corresponding pool
+  let mut mpools = pools.lock().await;
+  let pool = mpools.get_mut(&pkey).unwrap();
+  pool.insert(connection_id, tx);
+  info!("clock pool size {}", mpools.get(&pkey).unwrap().len());
+  // drop the lock before entering the connection read block
+  drop(mpools);
+
+  while let Some(result) = ws_rx.next().await {
+    let _msg = match result {
+      Ok(msg) => msg,
+      Err(e) => {
+        warn!("websocket error: {}", e);
+        break;
+      }
+    };
+  }
+
+  info!("leaving clock {:?}", connection_id);
+  let mut pools = pools.lock().await;
+  let pool = pools.get_mut(&exit_key).unwrap();
+  pool.remove(&connection_id);
+}
+
 async fn ws_reader(ws: WebSocket, key: String, pools: Pools, db: Storage) {
   let mut data = Object {
     created: 0,
@@ -57,25 +113,14 @@ async fn ws_reader(ws: WebSocket, key: String, pools: Pools, db: Storage) {
   let fkey = &key;
   let pkey = key.clone();
   let exit_key = key.clone();
-
   let connection_id = NEXT_CONNECTION_ID.fetch_add(1, Ordering::Relaxed);
 
-  if key == "" {
-    // Clock connection
-    info!("subscription to the clock");
-    data = Object {
-      created: 0,
-      updated: 0,
-      data: now().to_string(),
-    };
-  } else {
-    info!("subscription on {}!", key);
-    let result = db.lock().unwrap().get(fkey).unwrap();
-    if result.is_some() {
-      let raw = &result.unwrap();
-      let serialized = String::from_utf8_lossy(raw);
-      data = serde_json::from_str(&serialized).unwrap();
-    }
+  info!("subscription to \"{}\"", key);
+  let result = db.lock().unwrap().get(fkey).unwrap();
+  if result.is_some() {
+    let raw = &result.unwrap();
+    let serialized = String::from_utf8_lossy(raw);
+    data = serde_json::from_str(&serialized).unwrap();
   }
 
   let (ws_tx, mut ws_rx) = ws.split();
@@ -86,7 +131,7 @@ async fn ws_reader(ws: WebSocket, key: String, pools: Pools, db: Storage) {
 
   let j = serde_json::to_string(&data);
   tx.send(Ok(Message::text(j.unwrap()))).ok();
-  info!("handshake {} sent", key);
+  info!("handshake of \"{}\" sent", key);
 
   // add connection to the corresponding pool
   let mut mpools = pools.lock().await;
@@ -99,7 +144,12 @@ async fn ws_reader(ws: WebSocket, key: String, pools: Pools, db: Storage) {
     let pool = mpools.get_mut(&key).unwrap();
     pool.insert(connection_id, tx);
   }
-  info!("pool size {}", mpools.get(&pkey).unwrap().len());
+  info!(
+    "\"{}\" pool size {}",
+    pkey,
+    mpools.get(&pkey).unwrap().len()
+  );
+  // drop the lock before entering the connection read block
   drop(mpools);
 
   while let Some(result) = ws_rx.next().await {
@@ -110,39 +160,22 @@ async fn ws_reader(ws: WebSocket, key: String, pools: Pools, db: Storage) {
         break;
       }
     };
-    // info!("received: {:?}", msg);
   }
 
+  info!("leaving: \"{}\" {:?}", exit_key, connection_id);
   let mut pools = pools.lock().await;
   let pool = pools.get_mut(&exit_key).unwrap();
   pool.remove(&connection_id);
 }
 
 fn storage() -> Storage {
-  // Database
-  let db = sled::open("db").unwrap();
-  let test = Object {
-    created: now(),
-    updated: 0,
-    data: encode("{\"data\": \"🎁\"}"),
-  };
-  let serialized = serde_json::to_string(&test).unwrap();
-  let result = db.insert("test", serialized.as_bytes());
-  let _status = match result {
-    Ok(_content) => {
-      info!("sled success");
-    }
-    Err(error) => {
-      error!("sled failed to insert: {0}", error.to_string());
-    }
-  };
-
+  // Wrap the database as an arc(mutex)
+  let db = sled::open("db").expect("open");
   return Arc::new(Mutex::new(db));
 }
 
 fn json_body() -> impl Filter<Extract = (WriteMessage,), Error = warp::Rejection> + Clone {
-  // When accepting a body, we want a JSON body
-  // (and to reject huge payloads)...
+  // When accepting a body, we want a JSON body, (and to reject huge payloads)
   warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
@@ -193,23 +226,22 @@ async fn _write(
   db: Storage,
   pools: Pools,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+  info!("writing \"{}\"", key);
   let pkey = key.clone();
   let data = storage_write(db, key, msg);
   let pools = pools.lock().await;
   for (socket_key, connections) in pools.iter() {
-    // info!("{}", key);
+    // TODO: glob pattern match
+    if socket_key.to_string() != pkey {
+      continue;
+    }
     for (_index, connection) in connections.iter() {
-      if socket_key.to_string() == pkey {
-        let j = serde_json::to_string(&data);
-        connection.send(Ok(Message::text(j.unwrap()))).ok();
-      }
+      let j = serde_json::to_string(&data);
+      connection.send(Ok(Message::text(j.unwrap()))).ok();
     }
   }
 
-  Ok(warp::reply::with_status(
-    "Added items to the grocery list",
-    http::StatusCode::CREATED,
-  ))
+  Ok(warp::reply::with_status("{}", http::StatusCode::CREATED))
 }
 
 pub async fn run() {
@@ -218,32 +250,43 @@ pub async fn run() {
   // Keep track of connections, key is usize, value
   // is a websocket sender.
   let pools: Pools = Arc::new(tokio::sync::Mutex::new(Pool::new()));
+  // initialize the pools with the clock connections list
+  let mut mpools = pools.lock().await;
+  mpools.insert("".to_string(), Connections::default());
+  drop(mpools);
 
   // https://users.rust-lang.org/t/why-closure-outlives-variables-owned-by-main/16725
   // even if the variable is Arc<Mutex> you need to clone
-  // let subscription_pools = pools.clone();
   let _db = storage();
   let _rest_read_db = _db.clone();
   let _rest_write_db = _db.clone();
   let _keys_db = _db.clone();
   let _rest_pools = pools.clone();
+  let _subscription_pools = pools.clone();
+  let _clock_pools = pools.clone();
   // db filters cloning
   let subscription_db = warp::any().map(move || _db.clone());
   let rest_read_db = warp::any().map(move || _rest_read_db.clone());
   let rest_write_db = warp::any().map(move || _rest_write_db.clone());
   let keys_db = warp::any().map(move || _keys_db.clone());
   // Turn our "state" into a new Filter...
+  let subscription_pools = warp::any().map(move || _subscription_pools.clone());
   let rest_write_pools = warp::any().map(move || _rest_pools.clone());
-  let subscription_pools = warp::any().map(move || pools.clone());
+  let clock_pools = warp::any().map(move || _clock_pools.clone());
 
   // routes
   let subscription = warp::path::param()
-    // The `ws()` filter will prepare the Websocket handshake.
     .and(warp::ws())
     .and(subscription_pools)
     .and(subscription_db)
     .map(|key: String, ws: warp::ws::Ws, pools: Pools, db: Storage| {
       ws.on_upgrade(move |websocket| ws_reader(websocket, key, pools, db))
+    });
+
+  let clock = warp::ws()
+    .and(clock_pools)
+    .map(|ws: warp::ws::Ws, pools: Pools| {
+      ws.on_upgrade(move |websocket| clock_reader(websocket, pools))
     });
 
   let read =
@@ -266,10 +309,10 @@ pub async fn run() {
     // iterating over keys https://github.com/spacejam/sled/blob/master/tests/test_tree.rs#L306
     let mut result = db.lock().unwrap().iter().keys();
     let mut keys: Vec<String> = vec![];
+    info!("iterating through keys");
     loop {
       let _k = match result.next() {
         Some(_content) => {
-          info!("sled iterating");
           let raw = &_content.unwrap();
           let serialized = String::from_utf8_lossy(raw);
           keys.push(serialized.to_string());
@@ -282,7 +325,8 @@ pub async fn run() {
     warp::reply::json(&keys)
   });
 
-  let routes = subscription.or(write).or(read).or(keys);
+  let routes = subscription.or(clock).or(write).or(read).or(keys);
 
+  tokio::spawn(async { clock_writer(pools).await });
   warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
--- tests/http.rs
+++ tests/http.rs
@@ -1,5 +1,6 @@
+// use base64::encode;
 use hyper::body::HttpBody;
-use hyper::Client;
+use hyper::{Body, Client, Request};
 use tokio::time::delay_for;
 use tokio::time::Duration;
 
@@ -10,12 +11,30 @@ async fn start_server() {
 }
 
 #[tokio::test]
-async fn test_route() {
+async fn test_route_get() {
   let client = Client::new();
   let uri = "http://localhost:3030/test".parse().unwrap();
   let mut resp = client.get(uri).await.unwrap();
   println!("Response test: {}", resp.status());
   assert_eq!(resp.status(), 200);
+  while let Some(chunk) = resp.body_mut().data().await {
+    println!("{:?}", &chunk.unwrap())
+  }
+}
+
+#[tokio::test]
+async fn test_route_post() {
+  let client = Client::new();
+  let uri: String = "http://localhost:3030/test".parse().unwrap();
+  let req = Request::builder()
+    .method("POST")
+    .uri(uri)
+    .body(Body::from("{\"data\": \"🛷\"}"))
+    .expect("request builder");
+  println!("test post");
+  let mut resp = client.request(req).await.unwrap();
+  println!("Response post test: {}", resp.status());
+  assert_eq!(resp.status(), 201);
   while let Some(chunk) = resp.body_mut().data().await {
     println!("{:?}", &chunk.unwrap())
   }
